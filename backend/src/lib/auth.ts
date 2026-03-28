55050875b332b4d31e2f5a6ed2fc318e43415be4
siam9192 <ahsiam999@gmail.com>
2026-03-28 18:33:25 +0600

    update env

--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -2,12 +2,13 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { transporter } from "./mailer.js";
 import { prisma } from "./prisma.js";
+import { dash } from "@better-auth/infra";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
-
+ plugins:[dash()],
   session: {
     cookieCache: {
       enabled: true,
